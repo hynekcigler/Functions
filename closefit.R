835986098e9f6079b650182ad4179039b7abc578
Hynek Cígler <hynek.cigler@mail.muni.cz>
2018-12-11 13:24:49 +0100

    Update closefit.R

--- closefit.R
+++ closefit.R
@@ -1,6 +1,9 @@
-closefit <- function(m0, m1) {
+closefit <- function(m0, m1, df.fix=0) {
   ## This function compute indexes of the close fit of IRT models produced by the lme4 packages.
   ## m0, m1 are the the object of the class merMod
+  ## df.fix = glmer function needs to have at least one random effect. If there is no random effect
+  ##          in null model, you have to include "phantom" random effect alongside the fixed effect,
+  ##          e.g. resp ~ 1 + items + (1 | items) . In such, case, set df.fix =1
   ## NOTE: The indices are not based on M2 statistics as was suggested 
   ##       by Maydeu-Olivares and Joe (2006) or Cai and Hansen (2013), 
   ##       but it is based purely on the model deviance. This indices
@@ -15,12 +18,12 @@ closefit <- function(m0, m1) {
   
   m0 <- summary(m0)
   m1 <- summary(m1)
-  cfi <- ((m0$AICtab["deviance"]-m0$AICtab["df.resid"])-
+  cfi <- ((m0$AICtab["deviance"]-(m0$AICtab["df.resid"]-df.fix))-
             (m1$AICtab["deviance"]-m1$AICtab["df.resid"]))/
-    (m0$AICtab["deviance"]-m0$AICtab["df.resid"])
-  tli <- ((m0$AICtab["deviance"]/m0$AICtab["df.resid"])-
+    (m0$AICtab["deviance"]-(m0$AICtab["df.resid"]-df.fix))
+  tli <- ((m0$AICtab["deviance"]/(m0$AICtab["df.resid"]-df.fix))-
             (m1$AICtab["deviance"]/m1$AICtab["df.resid"]))/
-    (m0$AICtab["deviance"]/m0$AICtab["df.resid"]-1)
+    (m0$AICtab["deviance"]/(m0$AICtab["df.resid"]-df.fix)-1)
   if (m1$AICtab["deviance"] - m1$AICtab["df.resid"] < 0) {
     rmsea <- NaN
     warning("Chi2 is less than number of df - perfect fit. Fit indices could be biased", call.=F)
